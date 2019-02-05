08399b91c8fdd623d4db72d895b5bf278143fe8b
keziah <k.milligan.1@research.gla.ac.uk>
2019-02-05 13:16:37 +0000

    updating example.py

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ Example file to show the use of SavePlot and SaveLegend.
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from save_plot import SavePlot, SaveLegend
 
 # make save_plot objects
@@ -19,19 +20,31 @@ amp = [0.5, 1, 0.75, 0.25]
 c = ['darkorange','dodgerblue', 'firebrick', 'green']
 ls = ['-', '--', ':', '-.']
 mk = ['o', None, None, None]
-labels = list('{}Hz'.format(f) for f in freq)
+labels = list('{} rad/s'.format(f) for f in freq)
+
+# using seaborn style
+# feel free to omit this
+sns.set_style('darkgrid')
 
 # plot some waves
 for idx, f in enumerate(freq):
     plt.plot(x, amp[idx]*np.sin(f*x), color=c[idx], linestyle=ls[idx])
     
-# add stuff to the plot
-plt.grid()
+# label axes
 plt.ylabel('sin(x)')
-plt.xlabel('x')
+plt.xlabel('x (rad)')
+
+# set x-axis ticks and labels in fractions of pi
+# (I'm simply leaving some labels blank to aviod the hassle of proper 
+# matplotlib ticker stuff)
+xtx = np.linspace(0, 2*np.pi, 9)
+pi = '\u03c0'
+xtxlab = ['0', '', '{}/2', '', '{}', '', '3{}/2', '', '2{}']
+xtxlab = [item.format(pi) for item in xtxlab]
+plt.xticks(ticks=xtx, labels=xtxlab)
 
 # save the plot
 sp.plot(plt)
 
 # save the legend
-sl.plot(labels, colours=c, linestyles=ls, markers=mk, figsize=(1,1), ncol=1)
+sl.plot(labels, colours=c, linestyles=ls, markers=mk, figsize=(2,2), ncol=1)
